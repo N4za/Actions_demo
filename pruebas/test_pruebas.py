0ec1a73e976ae4c14a2ae12ac306067eac91b435
Nazareth <1718110400@utectulancingo.edu.mx>
2021-08-08 04:37:53 +0000

    test

---
+++ pruebas/test_pruebas.py
@@ -0,0 +1,14 @@
+import pytest
+import aplicacion as ar
+
+array = [5,3,4,2,1]
+diccionario = [
+    {"nombre":"Nazareth","edad":21},
+    {"nombre":"Yulissa","edad":15},
+    {"nombre":"Mariel","edad":30}]
+
+def test_pruebas():
+    assert ar.acomodo(array) == [1,2,3,4,5]
+
+def test_contar_mayores():
+    assert ar.contar_mayores(diccionario) == 2
